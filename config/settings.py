9b00d2a98ab68f97d556d203402b14001a39f15a
Abdurazzoq <rayyan.dev@mail.ru>
2023-05-16 22:58:38 +0500

    dadw

--- config/settings.py
+++ config/settings.py
@@ -110,8 +110,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+print(MEDIA_ROOT)
 
 if DEBUG:
     STATICFILES_DIRS = [
