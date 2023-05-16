326313a5607aa98637c3a58965ce1fc10da4d6eb
Abdurazzoq <rayyan.dev@mail.ru>
2023-05-16 22:33:53 +0500

    uyjygds

--- config/urls.py
+++ config/urls.py
@@ -5,31 +5,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-from django.http import HttpResponse
-
-from home.models import EducationCenter
-
-
-def asd(request):
-    ec = EducationCenter.objects.get(name='test')
-    qarz = 0
-
-    
-
-    return HttpResponse('<h1>Lorem ipsum</h1>')
-
-
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('day/', include('attendance.urls')),
+    path('api/', include('home.api')),
     path('', include('home.urls')),
 
-    path('api/', include('home.api')),
-
-    path('asd/', asd)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
